bd0e3ad460dfc33e2cbc0ed00bae63167cfbd3c7
Patrick Maul <maulpatrick1996@gmail.com>
2022-11-26 02:20:19 +0100

    feat: load readme

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <template lang="pug">
 navigationDrawer
+main#content
 </template>
 
 <script setup>
 import navigationDrawer from "./components/navigationDrawer.vue";
+import MarkdownIt from "markdown-it";
+import { onMounted } from "vue";
+import axios from "./axios.js";
+
+async function renderReadme() {
+    const data = (await axios.get("https://api.github.com/repos/patrickmaul/cheatsheet/readme")).data;
+
+    const md = new MarkdownIt();
+    document.querySelector("main#content").innerHTML = md.render(atob(data.content));
+}
+
+onMounted(() => {
+    document.addEventListener('DOMContentLoaded', () => {
+        renderReadme();
+    });
+});
 </script>
 
 <style lang="scss" scoped>
